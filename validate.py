5219fb8856786097506d03d02408be57e9fd7422
HAMZA-ZORO <116195770+HAMZA-ZORO@users.noreply.github.com>
2024-04-29 01:45:01 +0300

    validate.py

--- validate.py
+++ validate.py
@@ -1,21 +1,35 @@
 import requests
+
 def is_valid_subdomain(subdomain):
-    url = f"http://{subdomain}"  
+    url = f"http://{subdomain}"
     try:
         response = requests.get(url)
-        return response.status_code in [200]  
+        return response.status_code in [200, 403], response.status_code
     except requests.RequestException:
-        return False
+        return False, None
+
 def filter_valid_subdomains(filename):
-    valid_subdomains = []
+    valid_200_subdomains = []
+    valid_403_subdomains = []
     with open(filename, 'r') as file:
         for line in file:
             subdomain = line.strip()
-            if is_valid_subdomain(subdomain):
-                valid_subdomains.append(subdomain)
-    return valid_subdomains
+            is_valid, status_code = is_valid_subdomain(subdomain)
+            if is_valid:
+                if status_code == 200:
+                    valid_200_subdomains.append(subdomain)
+                elif status_code == 403:
+                    valid_403_subdomains.append(subdomain)
+    return valid_200_subdomains, valid_403_subdomains
+
 # Usage
-filename = input("enter the textFile name:")
-valid_subdomains = filter_valid_subdomains(filename)
-for subdomain in valid_subdomains:
-    print(subdomain)
+filename = input("Enter the text file name:")
+valid_200_subdomains, valid_403_subdomains = filter_valid_subdomains(filename)
+
+with open("valid_200_subdomains.txt", 'w') as file:
+    for subdomain in valid_200_subdomains:
+        file.write(subdomain + '\n')
+
+with open("valid_403_subdomains.txt", 'w') as file:
+    for subdomain in valid_403_subdomains:
+        file.write(subdomain + '\n')
